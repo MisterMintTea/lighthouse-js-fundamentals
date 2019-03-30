a46178a2e949dd6e85b401e7106bd56f6f9a73dc
MisterMintTea <victor.h.huynh@gmail.com>
2019-03-30 01:56:32 +0000

    Adds conditionals exercises

---
+++ go-outside.js
@@ -0,0 +1,10 @@
+var raining = false;
+var cold = true;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+} else if (cold) {
+  console.log("Make sure you pick out a scraf!");
+} else {
+  console.log("Now you're ready to go outside!");
+};
