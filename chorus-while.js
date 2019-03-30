dc6daf79b5c294590baf74550f3c00ce6cae3d03
Victor Huynh <victor.h.huynh@gmail.com>
2019-03-30 00:35:26 -0400

    Adds looping exercises

---
+++ chorus-while.js
@@ -0,0 +1,22 @@
+//While loop
+
+var chorus = "Let's dance!";
+var repeat = 0;
+while (repeat < 10) {
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun comes up!");
+
+// While & if loop
+
+var chorus = "Let's dance!";
+var repeat = 0;
+while (repeat < 10) {
+  if (repeat === 5) {
+    console.log("*change key*");
+  }
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun comes up!");
---
+++ packing-list.js
@@ -0,0 +1,8 @@
+// Iterating over an array
+
+var packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (var i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
